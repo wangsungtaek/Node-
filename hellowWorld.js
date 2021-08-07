707f959b5174dccd9b6fc71ecea4f3774a4c2d81
wangsungtaek <tjdxorv@naver.com>
2021-08-07 23:12:36 +0900

    Node 교과서 3장

---
+++ hellowWorld.js
@@ -0,0 +1,13 @@
+console.log('Hello World!');
+
+let a = 5;
+let b = 3;
+let c = a + b;
+function helloWorld() {
+    console.log('Hello World');
+    helloNode();
+}
+function helloNode() {
+    console.log('Hello Node');
+}
+helloWorld();
